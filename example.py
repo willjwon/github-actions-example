59057c224f6842cbde1a5b43ba3f95fb27975c7e
Will Won <william.won@gatech.edu>
2023-09-29 00:39:37 -0400

    Update example to Python and add unit tests

---
+++ example.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+class AreaCalculator:
+    @staticmethod
+    def triangle(base: int, height: int) -> float:
+        return base * height / 2.0
+
+    @staticmethod
+    def rectangle(width: int, height: int) -> int:
+        return width * height
+
+
+class TestAreaCalculator(unittest.TestCase):
+    def test_triangle(self):
+        area = AreaCalculator.triangle(10, 5)
+        self.assertEqual(area, 25.0)
+
+    def test_rectangle(self):
+        area = AreaCalculator.rectangle(10, 5)
+        self.assertEqual(area, 50)
