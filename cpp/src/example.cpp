df3e600f97c1d75a01b95436682e13e473c8c9ad
Will Won <william.won@gatech.edu>
2023-09-29 22:01:06 -0400

    Should fail

--- cpp/src/example.cpp
+++ cpp/src/example.cpp
@@ -1,7 +1,7 @@
 #include "example.h"
 
 int area_triangle(int base, int height) noexcept {
-    return base * height / 2;
+    return base * height * 2;
 }
 
 int area_rectangle(int width, int height) noexcept {
