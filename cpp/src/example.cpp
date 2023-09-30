e532f1ad6e91b0b2040755bd7acd2cab77ac9026
Will Won <william.won@gatech.edu>
2023-09-29 21:42:36 -0400

    Add C++ googletest example

---
+++ cpp/src/example.cpp
@@ -0,0 +1,9 @@
+#include "example.h"
+
+int area_triangle(int base, int height) noexcept {
+    return base * height / 2;
+}
+
+int area_rectangle(int width, int height) noexcept {
+    return width * height;
+}
---
+++ cpp/src/example.h
@@ -0,0 +1,7 @@
+#pragma once
+
+int area_triangle(int base, int height) noexcept;
+
+int area_rectangle(int width, int height) noexcept;
+
+
